1953aa90899ebb72b573a4d110c7247a1a4a1ed7
Vitalii-Egorov <monstermozes@gmail.com>
2024-10-11 19:02:55 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,120 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div class="container">
+      <h1>Список дел</h1>
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Добавить задачу"
+        @keyup.enter="addTask"
+      />
+      <button @click="addTask">Добавить</button>
+      <ul>
+        <li
+          v-for="(task, index) in tasks"
+          :key="index"
+          :class="{ completed: task.completed }" 
+        >
+        <div><input
+            type="checkbox"
+            v-model="task.completed"
+            @change="toggleTask(index)"
+          />
+          <span>{{ task.text }}</span></div>
+          
+         <div> <button @click="editTask(index)">Редактировать</button>
+          <button @click="deleteTask(index)">Удалить</button></div>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, computed } from 'vue';
+  
+  export default {
+    setup() {
+      // Вычисляемые свойства
+      const completedTasksCount = computed(() => {
+        return tasks.value.filter((task) => task.completed).length;
+      });
+  
+      // Функции
+      const addTask = () => {
+        if (newTask.value.trim() !== '') {
+          tasks.value.push({
+            text: newTask.value,
+            completed: false,
+          });
+          newTask.value = '';
+          saveTasksToLocalStorage();
+        }
+      };
+  
+      const toggleTask = (index) => {
+        tasks.value[index].completed = !tasks.value[index].completed;
+        tasks.value[index].completed = !tasks.value[index].completed;
+        saveTasksToLocalStorage();
+      };
+  
+      const editTask = (index) => {
+        const editedTask = prompt('Редактировать задачу:', tasks.value[index].text);
+        if (editedTask !== null) {
+          tasks.value[index].text = editedTask;
+          saveTasksToLocalStorage();
+        }
+      };
+  
+      const deleteTask = (index) => {
+        tasks.value.splice(index, 1);
+        saveTasksToLocalStorage();
+      };
+  
+      const loadTasksFromLocalStorage = () => {
+        const storedTasks = localStorage.getItem('tasks');
+        return storedTasks ? JSON.parse(storedTasks) : [];
+      };
+  
+      const saveTasksToLocalStorage = () => {
+        localStorage.setItem('tasks', JSON.stringify(tasks.value));
+      };
+     // Состояние задач
+      const tasks = ref(loadTasksFromLocalStorage());
+      const newTask = ref('');
+      return {
+        tasks,
+        newTask,
+        addTask,
+        toggleTask,
+        editTask,
+        deleteTask,
+        completedTasksCount,
+      };
+    },
+  };
+  </script>
+  
+  <style>
+  .container {
+    margin: 20px;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+   ul{
+    display: flex;
+    flex-direction: column-reverse
+   }
+  li {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  .completed {
+    text-decoration: line-through;
+    color: red; 
+  }
+  
+  </style>
+  
